cb94bade471a5c67fd95b5b7a7be0f26a9bf33f6
stanley355 <winatastanley355@gmail.com>
2022-02-03 15:39:40 +0700

    add head component

---
+++ src/components/Head/Head.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+
+interface HeadInterface {
+  title: string;
+  description: string;
+  keywords: string;
+}
+
+export const SiteHead = (props: { meta: HeadInterface }) => {
+  return (
+    <Head>
+      <meta name="title" content={props.meta.title} />
+      <meta name='keywords' content={props.meta.keywords}/>
+      <meta name='description' content={props.meta.description} />
+      <meta charSet="utf-8" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" charSet='UTF-8'/>
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+      <meta name="robots" content='index, follow, , max-snippet:-1, max-image-preview:large, max-video-preview:-1' />
+    </Head>
+  );
+
+}
