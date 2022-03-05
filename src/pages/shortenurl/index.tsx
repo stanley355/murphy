2e93dd491e57e2334b16fadeb6fa5368d5a7846e
stanley355 <winatastanley355@gmail.com>
2022-03-05 08:16:17 +0700

    add shortenurl texts

--- src/pages/shortenurl/index.tsx
+++ src/pages/shortenurl/index.tsx
@@ -1,8 +1,16 @@
 import React from "react"
 
+import styles from './Shortenurl.module.scss';
+
 const ShortenURL = () =>{
   return(
-    <div>This is shortenURL page</div>
+    <div className={styles.shortenurl}>
+      <div className={styles.shortenurl__texts}>
+        <h1>Short links, big results</h1>
+        <h2>A URL shortener built with powerful tools to help you grow and protect your brand.</h2>
+      </div>
+      <div>Put box here</div>
+    </div>
   )
 }
 
