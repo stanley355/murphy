4381ae8c2d75a13d904b3fb2c86f7338b4f0e2cf
stanley355 <winatastanley355@gmail.com>
2022-03-06 09:00:57 +0700

    add styling for shortulr form desktop

--- src/pages/shortenurl/index.tsx
+++ src/pages/shortenurl/index.tsx
@@ -1,12 +1,17 @@
 import React from "react"
+import classNames from "classnames";
 
 import ShorturlForm from "../../components/Forms/ShorturlForm/ShorturlForm";
 
+import useResponsive from "../../utils/useResponsive";
+
 import styles from './Shortenurl.module.scss';
 
-const ShortenURL = () =>{
-  return(
-    <div className={styles.shortenurl}>
+const ShortenURL = () => {
+  const { isDesktop } = useResponsive();
+
+  return (
+    <div className={classNames(styles.shortenurl, isDesktop ? "container" : "")}>
       <div className={styles.shortenurl__texts}>
         <h1>Short links, big results</h1>
         <h2>A URL shortener built with powerful tools to help you grow and protect your brand.</h2>
