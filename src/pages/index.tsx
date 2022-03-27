6c0e1d36b321faf2bfa169a4dc5bac2b349f66c1
stanley355 <winatastanley355@gmail.com>
2022-03-27 11:00:45 +0700

    remove redirection from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
-import Router from 'next/router';
-
+import React from 'react';
 import styles from '../../styles/pages/home.module.scss'
-
-import MetaHead from '../components/Head/Head';
 
 const Home = () => {
   const HomeMeta = {
@@ -11,10 +7,6 @@ const Home = () => {
     description: "Powerful and Fastest Open Source API Service written in Rust",
     keywords: "Shorten URL, API Service, Rust"
   }
-
-  useEffect(() =>{
-    Router.push('/shortenurl');
-  })
 
   return (
     <div className={styles.home}>
