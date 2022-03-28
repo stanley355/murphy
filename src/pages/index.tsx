f31082ed37f07a6594438c00311b23680e50f6eb
stanley355 <winatastanley355@gmail.com>
2022-03-28 11:13:41 +0700

    add hero content to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from '../clients/Homepage/Hero/Hero';
 import styles from '../../styles/pages/home.module.scss'
 
 const Home = () => {
@@ -10,7 +11,7 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-      
+      <Hero />
     </div>
   )
 }
