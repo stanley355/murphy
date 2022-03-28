1bf3ac9e14ee725a62df6d50c7f8799756f48da8
stanley355 <winatastanley355@gmail.com>
2022-03-28 08:04:39 +0700

    add dropdown

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
+import React, { useState } from 'react';
 import styles from './Navbar.module.scss';
 import { FaBars } from "react-icons/fa";
+
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const NavbarDropdown = () => {
+    return (
+      <div className={styles.navbar__dropdown}>
+
+      </div>
+    );
+  }
+
   return (
     <header className={styles.navbar}>
-      <a href="/" className={styles.navbar__logo}>Marph</a>
-      <a href='#docs' className={styles.navbar__menuBtn}><FaBars /> </a>
+      <a href="/" className={styles.navbar__logo}>Marph API</a>
+      <button className={styles.navbar__burgerBtn}><FaBars /></button>
     </header>
   );
 }
