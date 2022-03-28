f44a4eb68e1b34779e8d7dd72a0e4bf933931d7d
stanley355 <winatastanley355@gmail.com>
2022-03-28 08:40:33 +0700

    add navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,41 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import styles from './Navbar.module.scss';
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimesCircle } from "react-icons/fa";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
   const NavbarDropdown = () => {
     return (
-      <div className={styles.navbar__dropdown}>
-
+      <div
+        className={classNames(
+          styles.navbar__dropdown,
+          showDropdown ? styles.navbar__dropdown__show : ""
+        )}>
+        <div className={styles.navbar__dropdown__top}>
+          <a href="/" className={styles.navbar__dropdown__top__logo}>Marph API</a>
+          <button
+            className={styles.navbar__dropdown__top__closeBtn}
+            onClick={() => setShowDropdown(false)}
+          >
+            <FaTimesCircle />
+          </button>
+        </div>
       </div>
     );
   }
 
   return (
     <header className={styles.navbar}>
+      <NavbarDropdown />
       <a href="/" className={styles.navbar__logo}>Marph API</a>
-      <button className={styles.navbar__burgerBtn}><FaBars /></button>
+      <button
+        className={styles.navbar__burgerBtn}
+        onClick={() => setShowDropdown(true)}
+      >
+        <FaBars />
+      </button>
     </header>
   );
 }
