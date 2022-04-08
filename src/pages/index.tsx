57cfe43627be16251e65af47e78605f662cc2b45
stanley355 <winatastanley355@gmail.com>
2022-04-08 14:06:16 +0700

    folder restructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import MetaHead from '../components/Head/Head';
 import HomepageMeta from '../../public/seo/homepage.json';
-import MorphsearchCopy from '../clients/Homepage/MorphsearchCopy/MorphsearchCopy';
-import ShortenurlCopy from '../clients/Homepage/ShortenurlCopy/ShortenurlCopy';
-import Hero from '../clients/Homepage/Hero/Hero';
+import MorphsearchCopy from '../clients/pages/home/MorphsearchCopy/MorphsearchCopy';
+import ShortenurlCopy from '../clients/pages/home/ShortenurlCopy/ShortenurlCopy';
+import Hero from '../clients/pages/home/Hero/Hero';
 
 const Home = () => {
   return (
