e58d2fc82a0e27cbb5189c69cd05b38cb6761f0f
stanley355 <winatastanley355@gmail.com>
2022-04-08 16:43:49 +0700

    Cloud host list component

--- src/pages/clouds/hosts/index.tsx
+++ src/pages/clouds/hosts/index.tsx
@@ -1,48 +1,22 @@
 import React from 'react';
-import Link from 'next/link';
 import getConfig from 'next/config';
 import { GetStaticProps } from 'next';
-import { FaLink, FaDiceD6 } from 'react-icons/fa'
 
+import HostsList from '../../../clients/pages/clouds/hosts/list';
 import styles from './hosts.module.scss';
 import RestClient from '../../../lib/RestClient';
 
 const { BASE_URL } = getConfig().publicRuntimeConfig;
 
-const Hosts = ({ host_list }: any) => {
+const Hosts = ({ hostsList }: any) => {
   return (
     <div className={styles.hosts}>
       <div className="container">
-        <div className={styles.hosts__list}>
-          {host_list && host_list.map((host: any) =>
-            <div key={host.name} className={styles.hosts__list__card}>
-              <div className={styles.hosts__list__card__imgContainer}>
-                <img
-                  src={`/images/hosts/${host.name.toLowerCase()}.webp`}
-                  alt={host.name}
-                  width={50}
-                  height={50}
-                />
-              </div>
-              {/* <div>{host.name}</div> */}
-              <Link href={host.url}>
-                <a title={host.name} className={styles.hosts__list__card__websiteLink}>
-                  <FaLink /> Website
-                </a>
-              </Link>
-              <Link href={`/clouds/hosts/${host.name.toLowerCase()}`}>
-                <a title={host.name} className={styles.hosts__list__card__productLink}>
-                  <FaDiceD6 /> Products
-                </a>
-              </Link>
-            </div>
-          )}
-        </div>
+        <HostsList list={hostsList} />
       </div>
     </div>
   )
 }
-
 
 export const getStaticProps: GetStaticProps = async () => {
   const config = {
@@ -53,7 +27,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: {
-      host_list: data
+      hostsList: data
     }
   }
 }
