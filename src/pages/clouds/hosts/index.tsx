60cc4c203684fbcf16d20b3f1fac14cc08933c34
stanley355 <winatastanley355@gmail.com>
2022-04-08 16:15:47 +0700

    host page cards

--- src/pages/clouds/hosts/index.tsx
+++ src/pages/clouds/hosts/index.tsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import Link from 'next/link';
 import getConfig from 'next/config';
 import { GetStaticProps } from 'next';
+import { FaLink, FaDiceD6 } from 'react-icons/fa'
+
+import styles from './hosts.module.scss';
 import RestClient from '../../../lib/RestClient';
 
 const { BASE_URL } = getConfig().publicRuntimeConfig;
 
 const Hosts = ({ host_list }: any) => {
   return (
-    <div>
-      <div >
-        {host_list.map((host: any) =>
-          <div key={host.name}>
-            <div>{host.name}</div>
-            <div>{host.description}</div>
-            <div>{host.url}</div>
-          </div>
-        )}
+    <div className={styles.hosts}>
+      <div className="container">
+        <div className={styles.hosts__list}>
+          {host_list && host_list.map((host: any) =>
+            <div key={host.name} className={styles.hosts__list__card}>
+              <div className={styles.hosts__list__card__imgContainer}>
+                <img
+                  src={`/images/hosts/${host.name.toLowerCase()}.webp`}
+                  alt={host.name}
+                  width={50}
+                  height={50}
+                />
+              </div>
+              {/* <div>{host.name}</div> */}
+              <Link href={host.url}>
+                <a title={host.name} className={styles.hosts__list__card__websiteLink}>
+                  <FaLink /> Website
+                </a>
+              </Link>
+              <Link href={`/clouds/hosts/${host.name.toLowerCase()}`}>
+                <a title={host.name} className={styles.hosts__list__card__productLink}>
+                  <FaDiceD6 /> Products
+                </a>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
