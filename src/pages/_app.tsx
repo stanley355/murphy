687e53a5e9702c09241ee7fcb87af939c25ac740
stanley355 <winatastanley355@gmail.com>
2022-04-09 08:23:23 +0700

    add google analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,44 @@
 import '../../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import getConfig from 'next/config';
 import Layout from '../components/Layout/Layout';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const { 
+  FIREBASE_APIKEY, 
+  FIREBASE_AUTHDOMAIN, 
+  FIREBASE_PROJECTID, 
+  FIREBASE_STORAGEBUCKET, 
+  FIREBASE_MESSAGINGSENDERID, 
+  FIREBASE_APPID, 
+  FIREBASE_MEASUREMENTID 
+} = getConfig().publicRuntimeConfig;
+
 function MyApp({ Component, pageProps }: AppProps) {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: FIREBASE_APIKEY,
+    authDomain: FIREBASE_AUTHDOMAIN,
+    projectId: FIREBASE_PROJECTID,
+    storageBucket: FIREBASE_STORAGEBUCKET,
+    messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+    appId: FIREBASE_APPID,
+    measurementId: FIREBASE_MEASUREMENTID
+  };
+
+  // Initialize Firebase
+  if (typeof window !== 'undefined') {
+    const app = initializeApp(firebaseConfig);
+    getAnalytics(app);
+  }
+
+
   return (
     <Layout>
       <Component {...pageProps} />
