91e34b1122ce77e5e61721ad7972fab1e497aab1
stanley355 <winatastanley355@gmail.com>
2022-04-18 20:39:37 +0700

    new navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,61 +1,23 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.scss';
-import { FaBars, FaTimesCircle, FaLink, FaSearch, FaCloud } from "react-icons/fa";
+import { FaBars, FaCloud } from "react-icons/fa";
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const NavbarDropdown = () => {
-    return (
-      <div className={styles.navbar__dropdown}>
-        <div className={styles.navbar__dropdown__top}>
-          <a href="/" title="Marph API" className={styles.navbar__dropdown__top__logo}>Marph</a>
-          <button
-            className={styles.navbar__dropdown__top__closeBtn}
-            onClick={() => setShowDropdown(false)}
-          >
-            <FaTimesCircle />
-          </button>
-        </div>
-        <div className={styles.navbar__dropdown__bottom}>
-          <Link href="/shortenurl">
-            <a title='Shorten URL' onClick={() => setShowDropdown(false)}>
-              <FaLink />
-              Shorten URL
-            </a>
-          </Link>
-          <Link href="/morphsearch">
-            <a title='Search API' onClick={() => setShowDropdown(false)}>
-              <FaSearch />
-              Morph Search
-            </a>
-          </Link>
-          <Link href="/clouds">
-            <a title='Clouds Hosting Service' onClick={() => setShowDropdown(false)}>
-              <FaCloud />
-              Clouds Hosting Calculator
-            </a>
-          </Link>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <header className={styles.navbar}>
-      {showDropdown && <NavbarDropdown />}
-      <a
-        href="/"
-        title="Marph"
-        className={styles.navbar__logo} >
-        Marph
-      </a>
+      <Link href="/">
+        <a
+          title="Marph"
+          className={styles.navbar__logo} >
+          Marph
+        </a>
+      </Link>
       <button
-        className={styles.navbar__burgerBtn}
-        onClick={() => setShowDropdown(true)}
+        className={styles.navbar__cloudBtn}
       >
-        <FaBars />
+        <FaCloud />
       </button>
     </header>
   );
