3c4378213e897d82b00d6298b3d90a2b736aa23a
stanley355 <winatastanley355@gmail.com>
2022-04-18 20:32:17 +0700

    new homepage

--- src/clients/pages/landing/LandingHero/LandingHero.tsx
+++ src/clients/pages/landing/LandingHero/LandingHero.tsx
@@ -4,14 +4,19 @@ import styles from './LandingHero.module.scss';
 
 const LandingHero = () => {
   return (
+
     <div className={styles.landing__hero}>
-      <h1>Cloud Platforms</h1>
-      <h3>
-        Find the best cloud hosting
-        alternatives for your application,
-        websites, or online stores
-      </h3>
-      <button className={styles.landing__hero__cta}>Let's Go</button>
+      <div className='container'>
+        <div className={styles.landing__hero__content}>
+          <h1>Cloud Platforms</h1>
+          <h3>
+            Find the best cloud hosting
+            alternatives for your application,
+            websites, or online stores
+          </h3>
+          <button className={styles.landing__hero__cta}>Let's Go</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
-
-import styles from '../../styles/pages/landing.module.scss'
-
 import LandingHero from '../clients/pages/landing/LandingHero/LandingHero';
-import MetaHead from '../components/Head/Head';
-import HomepageMeta from '../../public/seo/homepage.json';
-import MorphsearchCopy from '../clients/pages/homepage/MorphsearchCopy/MorphsearchCopy';
-import ShortenurlCopy from '../clients/pages/homepage/ShortenurlCopy/ShortenurlCopy';
-import Hero from '../clients/pages/homepage/Hero/Hero';
 
 
 const Home = () => {
   return (
-    <ParallaxProvider>
-      <Parallax className={styles.landing__hero}>
-        <div className='container'>
-          <LandingHero />
-        </div>
-      </Parallax>
-    </ParallaxProvider>
+    <div>
+      <LandingHero />
+    </div>
   )
 }
 
