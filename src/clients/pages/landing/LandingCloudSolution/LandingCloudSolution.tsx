c5b93aad3b64e62dbe277447e2938423a4d32517
stanley355 <winatastanley355@gmail.com>
2022-04-19 08:46:15 +0700

    add cloud solution copywriting on landing page

---
+++ src/clients/pages/landing/LandingCloudSolution/LandingCloudSolution.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './LandingCloudSolution.module.scss';
+
+import { FaGripLines } from 'react-icons/fa';
+
+const LandingCloudSolution = () => {
+  return (
+    <div className={styles.landing__cloudsolution}>
+      <div className="container">
+        <h2>Cloud Solutions</h2>
+        <div className={styles.landing__cloudsolution__subtitle}>Marph offers two ways to solve your Cloud Hosting Problems:</div>
+
+        <div className={styles.landing__cloudsolution__list}>
+          <Link href="/plans">
+            <a title="Cloud Products" className={styles.landing__cloudsolution__card}>
+              <img
+                src="/images/landing/cloud-solution-products.webp"
+                alt="Cloud Products"
+              />
+              <div className={styles.landing__cloudsolution__card__title}>Product/Plan Solution</div>
+              <div className={styles.landing__cloudsolution__card__subtitle}>Compare various cloud products or plans from Cloud Hosting Services</div>
+            </a>
+          </Link>
+          <div className={styles.landing__cloudsolution__or}><FaGripLines /> <FaGripLines /> OR <FaGripLines /> <FaGripLines /></div>
+          {/* TODO: Change the href after we made the page */}
+          <Link href="/calculation" >
+            <a title="Cloud Recommendation" className={styles.landing__cloudsolution__card}>
+              <img
+                src="/images/landing/cloud-solution-checklist.webp"
+                alt="Cloud Recommendation"
+              />
+              <div className={styles.landing__cloudsolution__card__title}>Recommended Solution</div>
+              <div className={styles.landing__cloudsolution__card__subtitle}>Fill in your application needs and pick the most suitable Cloud from our recommendation</div>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingCloudSolution;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LandingHero from '../clients/pages/landing/LandingHero/LandingHero';
-
+import LandingCloudSolution from '../clients/pages/landing/LandingCloudSolution/LandingCloudSolution';
 
 const Home = () => {
   return (
     <div>
       <LandingHero />
+      <LandingCloudSolution />
     </div>
   )
 }
