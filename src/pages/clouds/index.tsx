11be6d68c5a71bd30205b2923dd1d7f8f767cb64
stanley355 <winatastanley355@gmail.com>
2022-04-21 08:11:29 +0700

    cloud hero component

--- src/pages/clouds/index.tsx
+++ src/pages/clouds/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import getConfig from 'next/config';
 import { GetServerSideProps } from 'next';
 
-import HeroContent from '../../clients/pages/clouds/components/Hero/Hero';
+import CloudHero from '../../clients/pages/clouds/components/CloudHero/CloudHero';
 import CloudList from '../../clients/pages/clouds/components/CloudList/CloudList';
 import { setCloudFilterQuery } from '../../clients/pages/clouds/utils/setCloudFilterQuery';
 import styles from './clouds.module.scss';
@@ -14,11 +14,9 @@ const Hosts = ({ hostList }: any) => {
 
   return (
     <div className={styles.clouds}>
-      <HeroContent />
+      <CloudHero />
       <div className="container">
-        <div className={styles.clouds__listContainer}>
           <CloudList hosts={hostList} />
-        </div>
       </div>
     </div>
   )
