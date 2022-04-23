9c40eb1f9975fb8090c6a07a4529204c96b8893b
stanley355 <winatastanley355@gmail.com>
2022-04-23 17:17:26 +0700

    some mistakes

--- src/clients/pages/cloudslug/components/CloudSlugHead/CloudSlugHead.tsx
+++ src/clients/pages/cloudslug/components/CloudSlugHead/CloudSlugHead.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { slugify } from '../../../../utils/slugify';
+import { slugify } from '../../../../../utils/slugify';
 
 import styles from './CloudSlugHead.module.scss';
 
