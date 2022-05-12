7c650a7af89509fbe706eee815965d1d0cb6ffbe
stan <winatastanley355@gmail.com>
2022-05-12 15:53:52 +0700

    cloud plans

--- src/pages/clouds/[slug].tsx
+++ src/pages/clouds/[slug].tsx
@@ -6,6 +6,7 @@ import MetaHead from '../../components/Head/Head';
 import CloudSlugHead from '../../clients/pages/cloudslug/components/CloudSlugHead';
 import CloudSlugPlansMobile from '../../clients/pages/cloudslug/components/CloudSlugPlans/Mobile/';
 import CloudSlugPlansDesktop from '../../clients/pages/cloudslug/components/CloudSlugPlans/Desktop/';
+import CloudSlugSimilarPlans from '../../clients/pages/cloudslug/components/CloudSlugSimilarPlans';
 import { setCloudSlugMeta } from '../../clients/pages/cloudslug/modules/setCloudSlugMeta';
 import { capitalizeFirstLetter } from '../../utils/capitalizeFirstLetter';
 
@@ -15,13 +16,14 @@ import styles from './cloudslug.module.scss';
 
 const { BASE_URL } = getConfig().publicRuntimeConfig;
 
-const CloudSlug = ({ hostData, hostPlans }: any) => {
+const CloudSlug = (props: any) => {
+  const { hostData, hostPlans, allPlans } = props;
   const { isDesktop } = useResponsive();
 
   return (
     <div className={styles.cloudslug}>
       <MetaHead meta={setCloudSlugMeta(hostData)} />
-      <div className={styles.cloudslug__head}>
+      <div className={styles.cloudslug__top}>
         <div className="container">
           <div className={styles.cloudslug__hostbox}>
             <CloudSlugHead name={hostData.name} url={hostData.url} />
@@ -34,6 +36,7 @@ const CloudSlug = ({ hostData, hostPlans }: any) => {
           </div>
         </div>
       </div>
+      <CloudSlugSimilarPlans hostID={hostData.id} planList={allPlans} />
     </div>
   );
 };
@@ -43,24 +46,32 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const hostName = params && params.slug;
 
-  const singleHostConfig = {
+  const singleHostRequest = {
     method: 'GET',
     url: `${BASE_URL}/api/clouds/host-single/?hostname=${capitalizeFirstLetter(hostName)}`,
   };
 
-  const hostData = await RestClient(singleHostConfig, {});
+  const hostData = await RestClient(singleHostRequest, {});
 
-  const hostPlanConfig = {
+  const hostPlanRequest = {
     method: 'GET',
     url: `${BASE_URL}/api/clouds/host-plans/?hostname=${capitalizeFirstLetter(hostName)}`,
   };
 
-  const hostPlans = await RestClient(hostPlanConfig, {});
+  const hostPlans = await RestClient(hostPlanRequest, {});
+
+  const allPlansRequest = {
+    method: 'GET',
+    url: `${BASE_URL}/api/clouds/plans/all/`,
+  };
+
+  const allPlansData = await RestClient(allPlansRequest, {});
 
   return {
     props: {
       hostData: hostData ?? null,
       hostPlans: hostPlans ?? [],
+      allPlans: allPlansData ?? []
     },
   };
 };
