3e08aa836baa4ae2714f0d2da540c10717293d93
Stanley Winata <winatastanley355@gmail.com>
2022-05-30 14:56:23 +0700

    fix firebase

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,30 +1,43 @@
-import React from 'react';
-import Head from 'next/head';
+import React, { useEffect } from 'react';
+import getConfig from 'next/config';
+
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const {
+  APP_ENV,
+  FIREBASE_APIKEY,
+} = getConfig().publicRuntimeConfig;
+
+
 const Layout = ({ children }: any) => {
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: FIREBASE_APIKEY,
+    authDomain: "marph-4bb98.firebaseapp.com",
+    projectId: "marph-4bb98",
+    storageBucket: "marph-4bb98.appspot.com",
+    messagingSenderId: "409398382775",
+    appId: "1:409398382775:web:08f10ead553b82e906fd32",
+    measurementId: "G-7S4L8WVV24"
+  };
+
+  // Initialize Firebase
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    if (APP_ENV === 'production') getAnalytics(app);
+  }, []);
 
   return (
     <div className="">
-      <Head>
-        {/* <!-- Google Tag Manager --> */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PB9J9XR');
-        `}}
-        />
-        {/* <!-- End Google Tag Manager --> */}
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PB9J9XR"
-            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
-      </Head>
       <Navbar />
       {children}
       <Footer />
