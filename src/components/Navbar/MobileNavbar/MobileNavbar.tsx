04ba4fd9ffe9447ef7619cbd307b9f1526ed3802
Stanley Winata <winatastanley355@gmail.com>
2022-06-01 11:15:53 +0700

    refact mobilenavbar

---
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { FaBuffer, FaTimes } from 'react-icons/fa';
+import Link from 'next/link';
+
+import styles from './MobileNavbar.module.scss';
+
+const MobileNavbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const MobileMenu = () => {
+        return (
+            <div className={styles.mobileNavbar__menu}>
+                <div className={styles.mobileNavbar__menu__head}>
+                    <Link href="/">
+                        <a
+                            title="Marph"
+                            className={styles.mobileNavbar__menu__logo} >
+                            Marph
+                        </a>
+                    </Link>
+                    <button className={styles.mobileNavbar__menu__closeBtn} onClick={() => setShowMobileMenu(false)}>
+                        <FaTimes />
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+
+    return (
+        <nav className={styles.mobileNavbar}>
+            <div className={styles.mobileNavbar__head}>
+                <Link href="/">
+                    <a
+                        title="Marph"
+                        className={styles.mobileNavbar__logo} >
+                        Marph
+                    </a>
+                </Link>
+                <button className={styles.mobileNavbar__menuBtn} onClick={() => setShowMobileMenu(true)}>
+                    <FaBuffer />
+                </button>
+            </div>
+            {showMobileMenu && <MobileMenu />}
+        </nav>
+    );
+}
+
+export default MobileNavbar;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
-import { FaBuffer } from "react-icons/fa";
 import styles from './Navbar.module.scss';
 
-import MobileMenu from './MobileMenu';
+import MobileNavbar from './MobileNavbar';
 
 const Navbar = () => {
-
-  const MobileNavbar = () => {
-    return (
-      <nav className={styles.navbar__mobileNav}>
-        <div className={styles.navbar__mobileNav__head}>
-          <Link href="/">
-            <a
-              title="Marph"
-              className={styles.navbar__mobileNav__logo} >
-              Marph
-            </a>
-          </Link>
-          <button className={styles.navbar__mobileNav__menuBtn}>
-            <FaBuffer />
-          </button>
-        </div>
-        <MobileMenu isOpen={true}/>
-      </nav>
-    );
-  }
 
   return (
     <header className={styles.navbar}>
