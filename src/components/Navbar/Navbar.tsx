3f6977b3e4e58238ecb04b7c9da29c23fa5ad286
Stanley Winata <winatastanley355@gmail.com>
2022-06-01 10:35:22 +0700

    refac mobile navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,18 +4,27 @@ import styles from './Navbar.module.scss';
 import { FaBuffer } from "react-icons/fa";
 
 const Navbar = () => {
+
+  const MobileNavbar = () => {
+    return (
+      <nav className={styles.navbar__mobileNav}>
+        <Link href="/">
+          <a
+            title="Marph"
+            className={styles.navbar__mobileNav__logo} >
+            Marph
+          </a>
+        </Link>
+        <button className={styles.navbar__mobileNav__menuBtn}>
+          <FaBuffer />
+        </button>
+      </nav>
+    );
+  }
+
   return (
     <header className={styles.navbar}>
-      <Link href="/">
-        <a
-          title="Marph"
-          className={styles.navbar__logo} >
-          Marph
-        </a>
-      </Link>
-      <button className={styles.navbar__menuBtn}>
-        <FaBuffer />
-      </button>
+      <MobileNavbar />
     </header>
   );
 }
