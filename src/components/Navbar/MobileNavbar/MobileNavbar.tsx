4628779b3c1d92f5a05d815821448633f5b41e7e
Stanley Winata <winatastanley355@gmail.com>
2022-06-01 11:23:52 +0700

    indent

--- src/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -5,45 +5,45 @@ import Link from 'next/link';
 import styles from './MobileNavbar.module.scss';
 
 const MobileNavbar = () => {
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
+	const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-    const MobileMenu = () => {
-        return (
-            <div className={styles.mobileNavbar__menu}>
-                <div className={styles.mobileNavbar__menu__head}>
-                    <Link href="/">
-                        <a
-                            title="Marph"
-                            className={styles.mobileNavbar__menu__logo} >
-                            Marph
-                        </a>
-                    </Link>
-                    <button className={styles.mobileNavbar__menu__closeBtn} onClick={() => setShowMobileMenu(false)}>
-                        <FaTimes />
-                    </button>
-                </div>
-            </div>
-        );
-    }
+	const MobileMenu = () => {
+		return (
+			<div className={styles.mobileNavbar__menu}>
+				<div className={styles.mobileNavbar__menu__head}>
+					<Link href="/">
+						<a
+							title="Marph"
+							className={styles.mobileNavbar__menu__logo} >
+							Marph
+						</a>
+					</Link>
+					<button className={styles.mobileNavbar__menu__closeBtn} onClick={() => setShowMobileMenu(false)}>
+						<FaTimes />
+					</button>
+				</div>
+			</div>
+		);
+	}
 
 
-    return (
-        <nav className={styles.mobileNavbar}>
-            <div className={styles.mobileNavbar__head}>
-                <Link href="/">
-                    <a
-                        title="Marph"
-                        className={styles.mobileNavbar__logo} >
-                        Marph
-                    </a>
-                </Link>
-                <button className={styles.mobileNavbar__menuBtn} onClick={() => setShowMobileMenu(true)}>
-                    <FaBuffer />
-                </button>
-            </div>
-            {showMobileMenu && <MobileMenu />}
-        </nav>
-    );
+	return (
+		<nav className={styles.mobileNavbar}>
+			<div className={styles.mobileNavbar__head}>
+				<Link href="/">
+					<a
+						title="Marph"
+						className={styles.mobileNavbar__logo} >
+						Marph
+					</a>
+				</Link>
+				<button className={styles.mobileNavbar__menuBtn} onClick={() => setShowMobileMenu(true)}>
+					<FaBuffer />
+				</button>
+			</div>
+			{showMobileMenu && <MobileMenu />}
+		</nav>
+	);
 }
 
 export default MobileNavbar;
