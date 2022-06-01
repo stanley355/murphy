1142899f7eb085d39b07c4f6eeb66f6b83c8758c
Stanley Winata <winatastanley355@gmail.com>
2022-06-01 11:00:49 +0700

    mobile menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './Navbar.module.scss';
 import { FaBuffer } from "react-icons/fa";
+import styles from './Navbar.module.scss';
+
+import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
 
   const MobileNavbar = () => {
     return (
       <nav className={styles.navbar__mobileNav}>
-        <Link href="/">
-          <a
-            title="Marph"
-            className={styles.navbar__mobileNav__logo} >
-            Marph
-          </a>
-        </Link>
-        <button className={styles.navbar__mobileNav__menuBtn}>
-          <FaBuffer />
-        </button>
+        <div className={styles.navbar__mobileNav__head}>
+          <Link href="/">
+            <a
+              title="Marph"
+              className={styles.navbar__mobileNav__logo} >
+              Marph
+            </a>
+          </Link>
+          <button className={styles.navbar__mobileNav__menuBtn}>
+            <FaBuffer />
+          </button>
+        </div>
+        <MobileMenu isOpen={true}/>
       </nav>
     );
   }
