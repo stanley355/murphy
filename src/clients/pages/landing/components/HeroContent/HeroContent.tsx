d7114ebce1f5580df5c04b59609e81f91bf0c8f4
Stanley Winata <winatastanley355@gmail.com>
2022-06-03 10:28:11 +0700

    hero content concept

---
+++ src/clients/pages/landing/components/HeroContent/HeroContent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HeroContent.module.scss';
+
+const HeroContent = () => {
+  return(
+    <div className={styles.heroContent}>
+      <h1>One Stop for All Web Services</h1>
+    </div>
+  );
+}
+
+export default HeroContent;
