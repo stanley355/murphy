3d36c5b42f8e1beadf568271308010fa0812b964
Stanley Winata <winatastanley355@gmail.com>
2022-06-10 10:17:33 +0700

    plan card

--- src/clients/pages/hosting/components/PlanList/PlanList.tsx
+++ src/clients/pages/hosting/components/PlanList/PlanList.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import styles from './PlanList.module.scss';
-
+import { setPlanAnalyticDisplay } from '../../../../common/modules/setPlanAnalyticDisplay';
+import { setPlanBandwidthDisplay } from '../../../../common/modules/setPlanBandwidthDisplay';
+import { setPlanConcurrentBuildDisplay } from '../../../../common/modules/setPlanConcurrentBuildDisplay';
+import { setPlanBuildDisplay } from '../../../../common/modules/setPlanBuildDisplay';
+import { setPlanPriceDisplay } from '../../../../common/modules/setPriceDisplay';
 interface PlanListInterface {
   plans: [any]
 }
@@ -11,25 +15,28 @@ const PlanList = (props: PlanListInterface) => {
 
 
   const PlanCard = (props: any) => {
-    const { plan } = props;
+    const { plan, key } = props;
 
     return (
-      <div key={plan.name}>
-        <div>
+      <div key={key} className={styles.planList__card}>
+        <div className={styles.planList__card__head}>
           <span>{plan.name}</span>
-          {/* <span>{setPlanPriceDisplay(plan)}</span> */}
+          <span>{setPlanPriceDisplay(plan)}</span>
         </div>
-        <div>{plan.description}</div>
-        <div></div>
-        <button>Check Out</button>
+        <div className={styles.planList__card__description}>{plan.description}</div>
+        <div className={styles.planList__card__spec}>Analytics: {setPlanAnalyticDisplay(plan)} </div>
+        <div className={styles.planList__card__spec}>Bandwidth: {setPlanBandwidthDisplay(plan)} </div>
+        <div className={styles.planList__card__spec}>Build: {setPlanBuildDisplay(plan)} </div>
+        <div className={styles.planList__card__spec}>Concurrent Build: {setPlanConcurrentBuildDisplay(plan)} </div>
+        <button className={styles.planList__card__cta}>Check Out</button>
       </div>
     );
   }
 
   return (
     <div className={styles.planList}>
-      <div>Hosting Plans</div>
-      {plans.map((plan: any) => <PlanCard plan={plan} />)}
+      <h2 className={styles.planList__title}>Hosting Plans</h2>
+      {plans.map((plan: any) => <PlanCard plan={plan} key={plan.name} />)}
     </div>
   )
 }
