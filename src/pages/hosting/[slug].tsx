85b0d9425d84b93cbfc45171ef83f67238c73daa
Stanley Winata <winatastanley355@gmail.com>
2022-06-13 14:56:16 +0700

    template logic

--- src/pages/hosting/[slug].tsx
+++ src/pages/hosting/[slug].tsx
@@ -32,14 +32,27 @@ const HostingSlug = (props: HostingSlugInterface) => {
     return <Skeleton />
   }
 
+  const Template = (props: any) => {
+    const { templateName } = props;
+
+    if (templateName === "Plan") {
+      return <PlanTemplate
+        hostData={hostData}
+        plansData={hostPlansData}
+        similarPlansData={similarPlansData}
+      />;
+    }
+
+    if (templateName === "Product") {
+      return <div>Hi</div>
+    }
+    return <div>404</div>
+  }
+
   return (
     <div className="hostingSlug">
       <div className="container">
-        <PlanTemplate
-          hostData={hostData}
-          plansData={hostPlansData}
-          similarPlansData={similarPlansData}
-        />
+        <Template templateName={hostData.template} />
         {newsData?.articles.length > 0 && <NewsCarousel
           carouselTitle="Related News"
           carouselItems={newsData.articles}
@@ -55,15 +68,17 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const hostName = params && capitalizeFirstLetter(params.slug);
   const hostData = await fetchSingleHost(hostName);
-  const planTemplateData = await fetchPlanTemplateData(hostName, hostData.id);
+  const planTemplateData = hostData.template === "Plan" ? await fetchPlanTemplateData(hostName, hostData.id) : null;
+
+
 
   const newsData = params && await fetchNews(params.slug);
 
   return {
     props: {
       hostData: hostData ?? null,
-      hostPlansData: planTemplateData.hostPlans ?? [],
-      similarPlansData: planTemplateData.similarPlans ?? [],
+      hostPlansData: planTemplateData?.hostPlans ?? [],
+      similarPlansData: planTemplateData?.similarPlans ?? [],
       newsData: newsData ?? []
     },
     revalidate: 2 * 60 // 2 minutes
