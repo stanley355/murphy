16a225fbad15f522c891d484c7a4bdcf7cd944a4
Stanley Winata <winatastanley355@gmail.com>
2022-06-20 10:25:24 +0700

    desktop product filter

--- src/clients/pages/hosting/components/ProductDisplay/ProductDisplay.tsx
+++ src/clients/pages/hosting/components/ProductDisplay/ProductDisplay.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import ProductTabs from '../ProductTabs';
 import styles from './ProductDisplay.module.scss';
-
+import ProductList from '../ProductList';
+import ProductTabs from '../ProductTabs';
+import ProductFilter from '../ProductFilter';
+import { filterProductList } from '../../modules/filterProductList';
 import useResponsive from '../../../../../utils/hooks/useResponsive';
 
 interface ProductDisplayInterface {
-  productList: [any]
+  productList: [any] | any[]
 }
 
 const ProductDisplay = (props: ProductDisplayInterface) => {
   const { productList } = props;
+  const [filteredProducts, setFilteredProducts] = useState(productList);
   const { isDesktop } = useResponsive();
+
+  const updateProductList = (filterValues: any) => {
+    const newProductList = filterProductList(filterValues, productList);
+    setFilteredProducts(newProductList);
+  }
+
+  const DesktopProductDisplay = () => {
+    return (
+      <div className={styles.productDisplay__desktop}>
+        <ProductList list={filteredProducts} />
+        <ProductFilter onSubmit={updateProductList} />
+      </div>
+    );
+  }
 
   return (
     <div className={styles.productDisplay}>
-      {!isDesktop && <ProductTabs productList={productList} />}
+      {isDesktop ? <DesktopProductDisplay /> : <ProductTabs productList={productList} />}
     </div>
   )
 }
