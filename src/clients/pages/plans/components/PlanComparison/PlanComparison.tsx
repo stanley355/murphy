967f18996386acc0409ff608cb47f15ea7df6336
Stanley Winata <winatastanley355@gmail.com>
2022-07-21 10:53:34 +0700

    Plan comparison refac

--- src/clients/pages/plans/components/PlanComparison/PlanComparison.tsx
+++ src/clients/pages/plans/components/PlanComparison/PlanComparison.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaCheck, FaTimes } from 'react-icons/fa';
+import Image from 'next/image';
 import Router from 'next/router';
 import styles from './PlanComparison.module.scss';
 
@@ -20,8 +21,8 @@ const PlanComparison = (props: IPlanComparison) => {
 
   return (
     <div className={styles.planComparison}>
-      <div className="rows">
-        <div className="img-wrap">-</div>
+      <div className={styles.planComparison__rowNames}>
+        <div className={styles.planComparison__imgWrap}>-</div>
         <div>Plan Name</div>
         <div>Free Domain</div>
         <div>Domain Extension</div>
@@ -32,8 +33,32 @@ const PlanComparison = (props: IPlanComparison) => {
         <div>Database Benefit</div>
         <div>Page Data</div>
         <div>Price</div>
-        <div className="button-wrap">-</div>
+        <div className={styles.planComparison__cta}>Checkout</div>
       </div>
+      {comparisonList.length > 0 && comparisonList.map((item: any) =>
+        < div className={styles.planComparison__rowValues}>
+          <div className={styles.planComparison__imgWrap}>
+            <Image
+              src={getPlanAvatarSrc(item.name)}
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className={styles.planComparison__title}>{item.name}</div>
+          <div>{item.free_domain ? <FaCheck /> : <FaTimes />}</div>
+          <div>{item.domain_extension ?? 'custom'}</div>
+          <div>{setPlanAnalyticDisplay(item)}</div>
+          <div>{setPlanBandwidthDisplay(item)}</div>
+          <div>{setPlanBuildDisplay(item)}</div>
+          <div>{setPlanConcurrentBuildDisplay(item)}</div>
+          <div>{item.database_benefit ? <FaCheck /> : <FaTimes />}</div>
+          <div>{item.page_data}</div>
+          <PlanPrice item={item} />
+          <div className={styles.planComparison__cta}>
+            <button onClick={()=> Router.push(item.plan_url)}>Purchase</button>
+            <button onClick={()=> onRemoveClick(item.id)}>Remove</button>
+          </div>
+        </div>)}
     </div>
   );
 }
